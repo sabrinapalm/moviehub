116dfc13f9689a455a131904dc8cde0ec390a547
sabrina palm <sabrina_palm@hotmail.com>
2018-06-15 01:06:23 +0200

    idk

--- main.js
+++ main.js
@@ -44,36 +44,80 @@ two.addEventListener('click', function(event){
 
 //GET MOVIE DATA
 function getMovies() {
-    ref.limitToFirst(8).on('value', function(snapshot) {
+    ref.on('value', function(snapshot) {
         let movieData = snapshot.val();
-        let keys = Object.keys(movieData);
+        let movieList = Object.values(movieData);
 
-        for (i = 0; i < keys.length; i++) {
-            let x = keys[i];
-            let movieT = movieData[x].title;
-            let movieD = movieData[x].director;
-            let movieY = movieData[x].year;
-            let movieI = movieData[x].img;
-            createMovie(movieT, movieD, movieY, movieI);
-        }
+        movieList.map(function(movie, index) {
+          let title = movie.title;
+          let director = movie.director;
+          let year = movie.year;
+          let img = movie.img;
+          let key = movie.id;
+          console.log()
+          createMovie(title, director, year, img, key);
+        });
+
     });
 }
 
 
-function pagination2() {
-  ref.limitToLast(8).on('value', function(snapshot) {
-      let movieData = snapshot.val();
-      let keys = Object.keys(movieData);
+//CREATE MOVIE BOX
+function createMovie(title, director, year, img, key) {
+    let div = document.createElement('div');
+    let image = document.createElement('img');
+    image.src = img;
+    let h4 = document.createElement('h4');
+    let p = document.createElement('p');
+    let removeBtn = document.createElement('button')
+    div.className = 'movies';
 
-      for (i = 0; i < keys.length; i++) {
-          let x = keys[i];
-          let movieT = movieData[x].title;
-          let movieD = movieData[x].director;
-          let movieY = movieData[x].year;
-          let movieI = movieData[x].img;
-          createMovie(movieT, movieD, movieY, movieI);
-      }
-  });
+    image.innerHTML = `<img src="${img}">`;
+    h4.innerText = `${title}`;
+    p.innerText = `${director}, ${year}`;
+    removeBtn.innerHTML = `<i class="fas fa-trash-alt"></i>`;
+    removeBtn.setAttribute("id", key)
+
+    removeBtn.className = 'removemovie';
+
+    div.appendChild(image);
+    div.appendChild(h4);
+    div.appendChild(p);
+    div.appendChild(removeBtn);
+
+    movie.appendChild(div);
+}
+
+//Remove movie from database
+document.addEventListener("click",function (event) {
+    if(event.target.className == "removemovie"){
+      var removeId = event.target.getAttribute("id");
+      removeMovie(removeId);
+    }
+  })
+
+removeMovie = (removeId) => {
+  console.log(removeId);
+  const database = firebase.database();
+  database.ref(`/movies/${removeId}`).remove();
+}
+
+
+//SORT MOVIES BY TITLE
+function sortMovies(title, director, year, img) {
+    movie.innerHTML = '';
+    var movieRef = database.ref().child('movies').orderByChild('title').limitToFirst(8);
+    movieRef.on('value', function(snapshot){
+        snapshot.forEach(function(item){
+            let newOrder = JSON.stringify(item.val());
+            let t = item.val().title;
+            let d = item.val().director;
+            let y = item.val().year;
+            let i = item.val().img;
+
+            createMovie(t, d, y, i);
+        })
+    })
 }
 
 
@@ -103,68 +147,6 @@ function clearField() {
     year.value = '';
     img.value = '';
 }
-
-
-
-//CREATE MOVIE BOX
-function createMovie(title, director, year, img) {
-    let div = document.createElement('div');
-    let image = document.createElement('img');
-    image.src = img;
-    let h4 = document.createElement('h4');
-    let p = document.createElement('p');
-    let removeBtn = document.createElement('button')
-    div.className = 'movies';
-
-    image.innerHTML = `<img src="${img}">`;
-    h4.innerText = `${title}`;
-    p.innerText = `${director}, ${year}`;
-    removeBtn.innerHTML = `<i class="fas fa-trash-alt"></i>`;
-
-    removeBtn.className = 'removemovie';
-
-    div.appendChild(image);
-    div.appendChild(h4);
-    div.appendChild(p);
-    div.appendChild(removeBtn);
-
-    movie.appendChild(div);
-}
-
-
-
-//SORT MOVIES BY TITLE
-function sortMovies(title, director, year, img) {
-    movie.innerHTML = '';
-    var movieRef = database.ref().child('movies').orderByChild('title').limitToFirst(8);
-    movieRef.on('value', function(snapshot){
-        snapshot.forEach(function(item){
-            let newOrder = JSON.stringify(item.val());
-            let t = item.val().title;
-            let d = item.val().director;
-            let y = item.val().year;
-            let i = item.val().img;
-
-            createMovie(t, d, y, i);
-        })
-    })
-}
-
-
-
-
-//REMOVE MOVIES
-function removeMovie() {
-    ref.on('child_removed', function(snapshot){
-        let data = snapshot.val();
-        let key = snapshot.key;
-    })
-}
-
-
-
-
-
 
 
 });
